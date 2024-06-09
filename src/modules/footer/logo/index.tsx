013115928aef9732be8d16b448427d99c4d2fd3d
Dzmitry Herashchanka <gera.1212@yandex.ru>
2024-06-09 18:02:54 +0300

    fix: исправляет разметку футера

--- src/modules/footer/logo/index.tsx
+++ src/modules/footer/logo/index.tsx
@@ -2,18 +2,24 @@ import { FC } from 'react'
 import Link from 'next/link'
 import LogoDesktop from '@icons/logo-full-footer.svg'
 import LogoMobile from '@icons/logo-mobile-footer.svg'
+import classNames from 'classnames'
 
 import styles from './logo.module.scss'
+import { LogoProps } from './logo.types'
 
-const Logo: FC = () => (
-  <Link href="/" className={styles.root} aria-label="home">
-    <span className={styles.desktop}>
-      <LogoDesktop />
-    </span>
-    <span className={styles.mobile}>
-      <LogoMobile />
-    </span>
-  </Link>
-)
+const Logo: FC<LogoProps> = ({ className }) => {
+  const rootClassName = classNames(styles.root, className)
+
+  return (
+    <Link href="/" className={rootClassName} aria-label="home">
+      <span className={styles.desktop}>
+        <LogoDesktop />
+      </span>
+      <span className={styles.mobile}>
+        <LogoMobile />
+      </span>
+    </Link>
+  )
+}
 
 export default Logo
--- src/modules/footer/social/index.tsx
+++ src/modules/footer/social/index.tsx
@@ -1,24 +1,29 @@
 import { FC } from 'react'
+import classNames from 'classnames'
 
 import styles from './social.module.scss'
 import { FooterSocialProps } from './social.types'
 
-const Social: FC<FooterSocialProps> = ({ items }) => (
-  <ul className={styles.root}>
-    {items.map(({ href, icon, label }) => (
-      <li className={styles.item} key={label}>
-        <a
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={label}
-          className={styles.link}
-        >
-          {icon}
-        </a>
-      </li>
-    ))}
-  </ul>
-)
+const Social: FC<FooterSocialProps> = ({ items, className }) => {
+  const rootClassName = classNames(styles.root, className)
+
+  return (
+    <ul className={rootClassName}>
+      {items.map(({ href, icon, label }) => (
+        <li className={styles.item} key={label}>
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={label}
+            className={styles.link}
+          >
+            {icon}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default Social
