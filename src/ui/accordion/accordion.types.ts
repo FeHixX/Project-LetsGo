e63dd27134cd33b70367a9956c176511cb0aad5f
VladSt <adrenaline2142@gmail.com>
2024-06-10 22:50:43 +0500

    add: make a basic accordion

---
+++ src/ui/accordion/accordion.types.ts
@@ -0,0 +1,8 @@
+export interface AccordionItemProps {
+  title: string;
+  content: string;
+}
+
+export interface AccordionProps {
+  items: AccordionItemProps[];
+}
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -3,12 +3,28 @@ import Image from 'next/image'
 import { Pagination } from '@/modules/pagination'
 import { Heading, Wrapper } from '@/ui'
 import classNames from 'classnames'
+import { Accordion } from '@/ui';
 
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
 
 const Home: FC<HomeProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
+  
+  const accordionItems = [
+    {
+      title: 'Первый',
+      content: 'Тест 1',
+    },
+    {
+      title: 'Второй',
+      content: 'Тест 2',
+    },
+    {
+      title: 'Третий',
+      content: 'Тест 3',
+    },
+  ];
 
   return (
     <main className={rootClassName}>
@@ -24,6 +40,7 @@ const Home: FC<HomeProps> = ({ className }) => {
           alt="Ligazavr"
           className={styles.image}
         />
+        <Accordion items={accordionItems} />
         <Pagination />
       </Wrapper>
     </main>
