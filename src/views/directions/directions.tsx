75ac05fbc44ef3a89c975da68c118f6bce7b0c08
VladSt <adrenaline2142@gmail.com>
2024-06-17 14:53:14 +0500

    add: styled calendar desktop

--- src/views/directions/directions.tsx
+++ src/views/directions/directions.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
 import { PageHeader } from '@/modules/pageHeader'
 import { User } from '@/modules/user'
+import { DatesOfStay } from '@/components'
 import classNames from 'classnames'
 
 import styles from './directions.module.scss'
 import { DirectionsProps } from './directions.types'
-import { DatesOfStay } from '@/components'
 
 const Directions: FC<DirectionsProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
@@ -14,6 +14,7 @@ const Directions: FC<DirectionsProps> = ({ className }) => {
     <main className={rootClassName}>
       <PageHeader className={styles.header}>Направления</PageHeader>
       <User />
+      <DatesOfStay></DatesOfStay>
     </main>
   )
 }
