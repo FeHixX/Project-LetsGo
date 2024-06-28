7a9b247c7e301dc8cab6afaa5a6f2718abc98bfa
Артемий <artema121rus@mail.ru>
2024-06-28 16:54:18 +0300

    Добавляет Блок - Маршрут без стилизации

--- src/components/countriesFilter/countriesFilter.tsx
+++ src/components/countriesFilter/countriesFilter.tsx
@@ -1,269 +1,84 @@
 'use client'
 
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import useResponsive from '@/shared/hooks/useResponsive'
+import axios from 'axios'
 
 import styles from './countriesFilter.module.scss'
 
 const continents = ['Европа', 'Азия', 'Америка', 'Острова']
-
-const countries = [
-  {
-    letter: 'А',
-    countries: [
-      'Австралия',
-      'Австрия',
-      'Азербайджан',
-      'Албания',
-      'Алжир',
-      'Ангола',
-      'Андорра',
-      'Антигуа и Барбуда',
-      'Аргентина',
-      'Армения',
-      'Афганистан'
-    ]
-  },
-  {
-    letter: 'Б',
-    countries: [
-      'Багамские Острова',
-      'Бангладеш',
-      'Барбадос',
-      'Бахрейн',
-      'Белоруссия',
-      'Белиз',
-      'Бельгия',
-      'Бенин',
-      'Болгария',
-      'Боливия',
-      'Босния и Герцеговина',
-      'Ботсвана',
-      'Бразилия',
-      'Бруней',
-      'Буркина-Фасо',
-      'Бурунди',
-      'Бутан'
-    ]
-  },
-  {
-    letter: 'В',
-    countries: [
-      'Вануату',
-      'Великобритания',
-      'Венгрия',
-      'Венесуэла',
-      'Восточный Тимор',
-      'Вьетнам'
-    ]
-  },
-  {
-    letter: 'Г',
-    countries: [
-      'Габон',
-      'Гаити',
-      'Гайана',
-      'Гамбия',
-      'Гана',
-      'Гватемала',
-      'Гвинея',
-      'Гвинея-Бисау',
-      'Германия',
-      'Гондурас',
-      'Гренада',
-      'Греция',
-      'Грузия'
-    ]
-  },
-  {
-    letter: 'Д',
-    countries: ['Дания', 'Джибути', 'Доминика', 'Доминикана']
-  },
-  {
-    letter: 'Е',
-    countries: ['Египет']
-  },
-  {
-    letter: 'З',
-    countries: ['Замбия', 'Зимбабве']
-  },
-  {
-    letter: 'И',
-    countries: [
-      'Израиль',
-      'Индия',
-      'Индонезия',
-      'Иордания',
-      'Ирак',
-      'Иран',
-      'Ирландия',
-      'Исландия',
-      'Испания',
-      'Италия'
-    ]
-  },
-  {
-    letter: 'К',
-    countries: [
-      'Кабо-Верде',
-      'Казахстан',
-      'Камбоджа',
-      'Камерун',
-      'Канада',
-      'Катар',
-      'Кения',
-      'Кипр',
-      'Киргизия',
-      'Кирибати',
-      'Китай',
-      'Колумбия'
-    ]
-  },
-  {
-    letter: 'Л',
-    countries: [
-      'Лаос',
-      'Латвия',
-      'Лесото',
-      'Либерия',
-      'Ливан',
-      'Ливия',
-      'Литва',
-      'Лихтенштейн',
-      'Люксембург'
-    ]
-  },
-  {
-    letter: 'М',
-    countries: [
-      'Маврикий',
-      'Мавритания',
-      'Мадагаскар',
-      'Малави',
-      'Малайзия',
-      'Мали',
-      'Мальдивы',
-      'Мальта',
-      'Марокко'
-    ]
-  },
-  {
-    letter: 'Н',
-    countries: [
-      'Намибия',
-      'Науру',
-      'Непал',
-      'Нигер',
-      'Нигерия',
-      'Нидерланды',
-      'Никарагуа',
-      'Новая Зеландия',
-      'Норвегия'
-    ]
-  },
-  {
-    letter: 'О',
-    countries: ['ОАЭ', 'Оман']
-  },
-  {
-    letter: 'П',
-    countries: [
-      'Пакистан',
-      'Палау',
-      'Панама',
-      'Папуа - Новая Гвинея',
-      'Парагвай',
-      'Перу',
-      'Польша',
-      'Португалия'
-    ]
-  },
-  {
-    letter: 'Р',
-    countries: ['Руанда', 'Румыния', 'Россия']
-  },
-  {
-    letter: 'С',
-    countries: [
-      'Сальвадор',
-      'Самоа',
-      'Сан-Марино',
-      'Саудовская Аравия',
-      'Сейшелы',
-      'Сенегал',
-      'Сербия',
-      'Сингапур',
-      'Сирия',
-      'Словакия',
-      'Словения',
-      'США'
-    ]
-  },
-  {
-    letter: 'Т',
-    countries: [
-      'Таджикистан',
-      'Таиланд',
-      'Танзания',
-      'Того',
-      'Тонга',
-      'Тринидад и Тобаго',
-      'Тувалу',
-      'Тунис',
-      'Туркмения',
-      'Турция'
-    ]
-  },
-  {
-    letter: 'У',
-    countries: ['Уганда', 'Узбекистан', 'Украина', 'Уругвай']
-  },
-  {
-    letter: 'Ф',
-    countries: ['Фиджи', 'Филиппины', 'Финляндия', 'Франция']
-  },
-  {
-    letter: 'Х',
-    countries: ['Хорватия']
-  },
-  {
-    letter: 'Ч',
-    countries: ['Чад', 'Черногория', 'Чехия', 'Чили']
-  },
-  {
-    letter: 'Ш',
-    countries: ['Швейцария', 'Швеция', 'Шри-Ланка']
-  },
-  {
-    letter: 'Э',
-    countries: ['Эквадор', 'Эритрея', 'Эсватини', 'Эстония', 'Эфиопия']
-  },
-  {
-    letter: 'Ю',
-    countries: ['ЮАР', 'Южный Судан']
-  },
-  {
-    letter: 'Я',
-    countries: ['Ямайка', 'Япония']
+interface Country {
+  name: { common: string; rus: string }
+  flags: {
+    png: string
+    svg: string
   }
-]
-
-let countriesAll: string[] = []
-countries.map((item) => {
-  for (let i = 0; i < item.countries.length; i++) {
-    const element = item.countries[i]
-    countriesAll.push(element)
-  }
-})
+  continent: string[]
+  island: boolean
+}
 
 const CountriesFilter: FC = () => {
   const [selectedContinent, setSelectedContinent] = useState('ЕВРОПА')
-  const [selectedLetter, setSelectedLetter] = useState('А')
+  const [countries, setCountries] = useState<Country[]>([])
+  const [lettersAndCountries, setLetterAndCountries] = useState([])
+  const [selectedLetter, setSelectedLetter] = useState<string>('А')
+  const [selectedCountries, setSelectedCountries] = useState<Country[]>([])
+
   const { isMobile, isTablet, isDesktop } = useResponsive()
   const [active, setActive] = useState<boolean>(false)
 
-  const filteredCountries: string[] = countriesAll.filter((country) =>
-    country.startsWith(selectedLetter)
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await axios.get(
+        'https://lets-go-8s43.onrender.com/countries/'
+      )
+      if (response.data && typeof response.data === 'object') {
+        const flattenedCountries = Object.values(
+          response.data
+        ).flat() as Country[]
+        const data = response.data
+        const countries = Object.entries(data).map((data) => ({
+          [data[0]]: data[1]
+        }))
+        setLetterAndCountries(countries)
+        setCountries(flattenedCountries)
+        console.log(countries)
+      }
+    }
+
+    fetchCountries()
+  }, [])
+
+  const letters = [
+    'А',
+    'Б',
+    'В',
+    'Г',
+    'Д',
+    'Е',
+    'З',
+    'И',
+    'К',
+    'Л',
+    'М',
+    'Н',
+    'О',
+    'П',
+    'Р',
+    'С',
+    'Т',
+    'У',
+    'Ф',
+    'Х',
+    'Ч',
+    'Ш',
+    'Э',
+    'Ю',
+    'Я'
+  ]
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.rus.startsWith(selectedLetter)
   )
 
   return (
@@ -324,7 +139,7 @@ const CountriesFilter: FC = () => {
       <div className={active ? styles.activecontainer : styles.container}>
         {isDesktop && (
           <div className={styles.wrapper}>
-            {countries.map((item) => (
+            {/* {countries.map((item) => (
               <div className={styles.countries} key={item.letter}>
                 <h4 className={styles.letter}>{item.letter}</h4>
                 <ul className={styles.list}>
@@ -335,26 +150,26 @@ const CountriesFilter: FC = () => {
                   ))}
                 </ul>
               </div>
-            ))}
+            ))} */}
           </div>
         )}
         {(isTablet || isMobile) && (
           <div className={styles.wrapper}>
             <div className={styles.letters}>
-              {countries.map((item) => (
+              {letters.map((letter) => (
                 <button
                   className={styles.letter}
-                  key={item.letter}
-                  onClick={() => setSelectedLetter(item.letter)}
+                  key={letter}
+                  onClick={() => setSelectedLetter(letter)}
                 >
-                  {item.letter}
+                  {letter}
                 </button>
               ))}
             </div>
             <ul className={styles.list}>
               {filteredCountries.map((country) => (
-                <li className={styles.country} key={country}>
-                  <span className={styles.countryText}>{country}</span>
+                <li key={country.name.common}>
+                  <span className={styles.countryText}>{country.name.rus}</span>
                 </li>
               ))}
             </ul>
--- src/components/stepTwoItinerary/stepTwoItinerary.tsx
+++ src/components/stepTwoItinerary/stepTwoItinerary.tsx
@@ -1,62 +1,107 @@
-import { FC, useState, useEffect } from 'react';
-import classNames from 'classnames';
-import Polygon from '@icons/polygon.svg';
-import axios from 'axios';
-import Image from 'next/image';
+import { FC, useEffect, useState } from 'react'
+import Image from 'next/image'
+import Select from '@/ui/select/select'
+import PolygonNext from '@icons/polygon-next.svg'
+import PolygonPrev from '@icons/polygon-prev.svg'
+import axios from 'axios'
+import classNames from 'classnames'
 
-import styles from './stepTwoItinerary.module.scss';
-import { StepTwoItineraryProps } from './stepTwoItinerary.types';
-import StepList from '../formNavigation/formNavigation';
+import StepList from '../formNavigation/formNavigation'
+import styles from './stepTwoItinerary.module.scss'
+import { StepTwoItineraryProps } from './stepTwoItinerary.types'
 
 interface Country {
-  name: { common: string; rus: string };
+  name: { common: string; rus: string }
   flags: {
-    png: string;
-    svg: string;
-  };
-  continent: string[];
-  island: boolean;
+    png: string
+    svg: string
+  }
+  continent: string[]
+  island: boolean
 }
 
-const StepTwoItinerary: FC<StepTwoItineraryProps> = ({ className, nextStep, prevStep }) => {
-  const rootClassName = classNames(styles.root, className);
+const StepTwoItinerary: FC<StepTwoItineraryProps> = ({
+  className,
+  nextStep,
+  prevStep
+}) => {
+  const rootClassName = classNames(styles.root, className)
 
   const handleNext = () => {
-    nextStep();
-  };
+    nextStep()
+  }
 
   const handlePrev = () => {
-    prevStep();
-  };
+    prevStep()
+  }
 
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [selectedCountries, setSelectedCountries] = useState<Country[]>([]);
-  const [selectedLetter, setSelectedLetter] = useState<string>('А');
+  const [countries, setCountries] = useState<Country[]>([])
+  const [selectedCountries, setSelectedCountries] = useState<Country[]>([])
+  const [selectedLetter, setSelectedLetter] = useState<string>('А')
+  const [activeSelect, setActiveSelect] = useState(false)
 
   useEffect(() => {
     const fetchCountries = async () => {
-      const response = await axios.get('https://lets-go-8s43.onrender.com/countries/');
+      const response = await axios.get(
+        'https://lets-go-8s43.onrender.com/countries/'
+      )
       if (response.data && typeof response.data === 'object') {
-        const flattenedCountries = Object.values(response.data).flat() as Country[];
-        setCountries(flattenedCountries);
+        const flattenedCountries = Object.values(
+          response.data
+        ).flat() as Country[]
+        setCountries(flattenedCountries)
       }
-    };
+    }
 
-    fetchCountries();
-  }, []);
+    fetchCountries()
+  }, [])
 
   const handleCountrySelect = (country: Country) => {
-    if (country && selectedCountries.length < 4 && !selectedCountries.some(c => c.name.common === country.name.common)) {
-      setSelectedCountries([...selectedCountries, country]);
+    if (
+      country &&
+      selectedCountries.length < 4 &&
+      !selectedCountries.some((c) => c.name.common === country.name.common)
+    ) {
+      setSelectedCountries([...selectedCountries, country])
     }
-  };
+  }
 
   const handleCountryRemove = (country: Country) => {
-    setSelectedCountries(selectedCountries.filter((c) => c.name.common !== country.name.common));
-  };
+    setSelectedCountries(
+      selectedCountries.filter((c) => c.name.common !== country.name.common)
+    )
+  }
 
-  const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'З', 'И', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ч', 'Ш', 'Э', 'Ю', 'Я'];
-  const filteredCountries = countries.filter(country => country.name.rus.startsWith(selectedLetter));
+  const letters = [
+    'А',
+    'Б',
+    'В',
+    'Г',
+    'Д',
+    'Е',
+    'З',
+    'И',
+    'К',
+    'Л',
+    'М',
+    'Н',
+    'О',
+    'П',
+    'Р',
+    'С',
+    'Т',
+    'У',
+    'Ф',
+    'Х',
+    'Ч',
+    'Ш',
+    'Э',
+    'Ю',
+    'Я'
+  ]
+  const filteredCountries = countries.filter((country) =>
+    country.name.rus.startsWith(selectedLetter)
+  )
 
   return (
     <div className={rootClassName}>
@@ -64,60 +109,109 @@ const StepTwoItinerary: FC<StepTwoItineraryProps> = ({ className, nextStep, prev
         <div className={styles.formHead}>
           <div className={styles.formDescription}>
             <h2>Шаг 2. Маршрут</h2>
-            <p>Укажите страны, которые вы хотели бы посетить. <br />
-              Это может быть одна или сразу несколько.</p>
+            <p>
+              Укажите страны, которые вы хотели бы посетить. <br />
+              Это может быть одна или сразу несколько.
+            </p>
           </div>
           <StepList currentStep={1} activeStep={1} setStep={nextStep} />
         </div>
-        <button onClick={handlePrev}>На шаг назад</button>
-        <button className={styles.formButton} onClick={handleNext}>Следующий шаг
-          <Polygon />
-        </button>
-      </div>
-      <div>
-        <select onChange={(e) => {
-          const value = e.target.value;
-          if (letters.includes(value)) {
-            setSelectedLetter(value);
-          } else {
-            const selectedCountry = countries.find(c => c.name.common === value);
-            if (selectedCountry) {
-              handleCountrySelect(selectedCountry);
-            }
-          }
-        }}>
-          <option value="">Выберите страну</option>
-          {letters.map(letter => (
-            <option key={letter} value={letter} style={{ fontWeight: 'bold' }}>
-              {letter}
-            </option>
-          ))}
-          {filteredCountries.map((country) => (
-            <option key={country.name.common} value={country.name.common}>
-              {country.name.rus} ({country.name.common})
-            </option>
-          ))}
-        </select>
-        <div>
-          <h3>Страны (Max 4):</h3>
-          <ul>
-            {selectedCountries.map((country) => (
-              <li key={country.name.common}>
-                <Image 
-                  src={country.flags.png} 
-                  alt={`${country.name.common} flag`} 
-                  width={20} 
-                  height={15} 
-                />
-                {country.name.rus} ({country.name.common})
-                <button onClick={() => handleCountryRemove(country)}>Удалить</button>
-              </li>
-            ))}
-          </ul>
+        <div className={styles.formContainer}>
+          <div>
+            <ul className={styles.countries}>
+              {selectedCountries.map((country) => (
+                <li className={styles.country} key={country.name.common}>
+                  <div className={styles.countryName}>
+                    {country.name.rus} ({country.name.common})
+                  </div>
+                  <div className={styles.countryIndicator}>
+                    <Image
+                      className={styles.countryFlags}
+                      src={country.flags.png}
+                      alt={`${country.name.common} flag`}
+                      width={70}
+                      height={47}
+                    />
+                    <button
+                      className={styles.countryDelete}
+                      onClick={() => handleCountryRemove(country)}
+                    ></button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.formSelectContainer}>
+            <div className={styles.formSelect}>
+              <button
+                onClick={() => setActiveSelect(!activeSelect)}
+                className={styles.formSelectButton}
+              >
+                <span className={styles.formSelectButtonText}>
+                  Выберите страну
+                </span>
+              </button>
+              <div
+                className={
+                  activeSelect
+                    ? styles.activeLettersAndCountries
+                    : styles.lettersAndCountries
+                }
+              >
+                <ul className={styles.letters}>
+                  {letters.map((letter) => (
+                    <li
+                      className={styles.letter}
+                      key={letter}
+                      onClick={() => setSelectedLetter(letter)}
+                      style={{ fontWeight: 'bold' }}
+                    >
+                      {letter}
+                    </li>
+                  ))}
+                </ul>
+                <ul className={styles.countries}>
+                  {filteredCountries.map((country) => (
+                    <li
+                      className={styles.country}
+                      key={country.name.common}
+                      onClick={() => {
+                        const selectedCountry = countries.find(
+                          (c) => c.name.common === country.name.common
+                        )
+                        if (selectedCountry) {
+                          handleCountrySelect(selectedCountry)
+                        }
+                      }}
+                    >
+                      {country.name.rus} ({country.name.common})
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <button className={styles.countryDelete}></button>
+          </div>
+        </div>
+        <div className={styles.formButtons}>
+          <button
+            className={`${styles.formButton} ${styles.formButtonNext}`}
+            onClick={handleNext}
+          >
+            Следующий шаг
+            <PolygonNext />
+          </button>
+          <button
+            className={`${styles.formButton} ${styles.formButtonPrev}`}
+            onClick={handlePrev}
+          >
+            <PolygonPrev />
+            На шаг назад
+          </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default StepTwoItinerary;
+export default StepTwoItinerary
