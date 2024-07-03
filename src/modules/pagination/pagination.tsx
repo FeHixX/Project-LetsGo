50d95c7e4979f0e8dd57fe3c56df67a221ec24c6
Vladislav Lushnikov <adrenaline2142@gmail.com>
2024-07-04 01:34:14 +0500

    add: pagination

--- src/modules/pagination/pagination.tsx
+++ src/modules/pagination/pagination.tsx
@@ -6,30 +6,40 @@ import classNames from 'classnames'
 import styles from './pagination.module.scss'
 import { PaginationProps } from './pagination.types'
 
-const Pagination: FC<PaginationProps> = ({ className }) => {
+const Pagination: FC<PaginationProps> = ({ className, currentPage, totalPages, onPageChange }) => {
   const rootClassName = classNames(styles.root, className)
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1)
+    }
+  }
+
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1)
+    }
+  }
 
   return (
     <div className={rootClassName}>
       <ul className={styles.list}>
-        <li>
-          <span className={styles.current}>1</span>
-        </li>
-        <li>
-          <a href="#">2</a>
-        </li>
-        <li>
-          <a href="#">3</a>
-        </li>
-        <li>
-          <a href="#">4</a>
-        </li>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <li key={page}>
+            <button
+              className={classNames(styles.pageButton, { [styles.current]: page === currentPage })}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
       </ul>
       <div className={styles.arrows}>
-        <button type="button" data-pagination="prev">
+        <button type="button" onClick={handlePrevClick} disabled={currentPage === 1}>
           <IconPrev />
         </button>
-        <button type="button" data-pagination="next">
+        <button type="button" onClick={handleNextClick} disabled={currentPage === totalPages}>
           <IconNext />
         </button>
       </div>
